b97f59389a30fad4b4dd14133fc9dae7384d02f1
Meghana Rao <meghanarao.99@gmail.com>
2020-11-22 15:55:40 +0530

    changed sql(shruthis)

--- online-examination-systen-in-php-master/project.sql
+++ online-examination-systen-in-php-master/project.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2017 at 11:50 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Generation Time: Nov 22, 2020 at 09:12 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -37,7 +38,6 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
-(1, 'sunnygkp10@gmail.com', '123456'),
 (2, 'admin@admin.com', 'admin');
 
 -- --------------------------------------------------------
@@ -56,26 +56,11 @@ CREATE TABLE `answer` (
 --
 
 INSERT INTO `answer` (`qid`, `ansid`) VALUES
-('55892169bf6a7', '55892169d2efc'),
-('5589216a3646e', '5589216a48722'),
-('558922117fcef', '5589221195248'),
-('55892211e44d5', '55892211f1fa7'),
-('558922894c453', '558922895ea0a'),
-('558922899ccaa', '55892289aa7cf'),
-('558923538f48d', '558923539a46c'),
-('55892353f05c4', '55892354051be'),
-('558973f4389ac', '558973f462e61'),
-('558973f4c46f2', '558973f4d4abe'),
-('558973f51600d', '558973f526fc5'),
-('558973f55d269', '558973f57af07'),
-('558973f5abb1a', '558973f5e764a'),
-('5589751a63091', '5589751a81bf4'),
-('5589751ad32b8', '5589751adbdbd'),
-('5589751b304ef', '5589751b3b04d'),
-('5589751b749c9', '5589751b9a98c');
+('5fba1c97c18a9', '5fba1c97c1fe6'),
+('5fba1c97c4b5a', '5fba1c97c50cf'),
+('5fba1c97c7468', '5fba1c97c828e');
 
 -- --------------------------------------------------------
-
 
 --
 -- Table structure for table `history`
@@ -88,7 +73,7 @@ CREATE TABLE `history` (
   `level` int(11) NOT NULL,
   `sahi` int(11) NOT NULL,
   `wrong` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -96,14 +81,7 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
-('sunnygkp10@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 09:31:26'),
-('sunnygkp10@gmail.com', '558920ff906b8', 4, 2, 2, 0, '2015-06-23 13:32:09'),
-('avantika420@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 14:33:04'),
-('avantika420@gmail.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 14:49:39'),
-('sunnygkp10@gmail.com', '5589741f9ed52', 4, 5, 3, 2, '2015-06-23 15:07:16'),
-('mi5@hollywood.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 15:12:56'),
-('nik1@gmail.com', '558921841f1ec', 1, 2, 1, 1, '2015-06-23 16:11:50'),
-('sunnygkp10@gmail.com', '5589222f16b93', 1, 2, 1, 1, '2015-06-24 03:22:38');
+('admin@admin.com', '5fba1bacc85f0', 3, 3, 3, 0, '2020-11-22 08:09:12');
 
 -- --------------------------------------------------------
 
@@ -122,75 +100,18 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
-('55892169bf6a7', 'usermod', '55892169d2efc'),
-('55892169bf6a7', 'useradd', '55892169d2f05'),
-('55892169bf6a7', 'useralter', '55892169d2f09'),
-('55892169bf6a7', 'groupmod', '55892169d2f0c'),
-('5589216a3646e', '751', '5589216a48713'),
-('5589216a3646e', '752', '5589216a4871a'),
-('5589216a3646e', '754', '5589216a4871f'),
-('5589216a3646e', '755', '5589216a48722'),
-('558922117fcef', 'echo', '5589221195248'),
-('558922117fcef', 'print', '558922119525a'),
-('558922117fcef', 'printf', '5589221195265'),
-('558922117fcef', 'cout', '5589221195270'),
-('55892211e44d5', 'int a', '55892211f1f97'),
-('55892211e44d5', '$a', '55892211f1fa7'),
-('55892211e44d5', 'long int a', '55892211f1fb4'),
-('55892211e44d5', 'int a$', '55892211f1fbd'),
-('558922894c453', 'cin>>a;', '558922895ea0a'),
-('558922894c453', 'cin<<a;', '558922895ea26'),
-('558922894c453', 'cout>>a;', '558922895ea34'),
-('558922894c453', 'cout<a;', '558922895ea41'),
-('558922899ccaa', 'cout', '55892289aa7cf'),
-('558922899ccaa', 'cin', '55892289aa7df'),
-('558922899ccaa', 'print', '55892289aa7eb'),
-('558922899ccaa', 'printf', '55892289aa7f5'),
-('558923538f48d', '255.0.0.0', '558923539a46c'),
-('558923538f48d', '255.255.255.0', '558923539a480'),
-('558923538f48d', '255.255.0.0', '558923539a48b'),
-('558923538f48d', 'none of these', '558923539a495'),
-('55892353f05c4', '192.168.1.100', '5589235405192'),
-('55892353f05c4', '172.168.16.2', '55892354051a3'),
-('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
-('55892353f05c4', '11.11.11.11', '55892354051be'),
-('558973f4389ac', 'containing root file-system required during bootup', '558973f462e44'),
-('558973f4389ac', ' Contains only scripts to be executed during bootup', '558973f462e56'),
-('558973f4389ac', ' Contains root-file system and drivers required to be preloaded during bootup', '558973f462e61'),
-('558973f4389ac', 'None of the above', '558973f462e6b'),
-('558973f4c46f2', 'Kernel', '558973f4d4abe'),
-('558973f4c46f2', 'Shell', '558973f4d4acf'),
-('558973f4c46f2', 'Commands', '558973f4d4ad9'),
-('558973f4c46f2', 'Script', '558973f4d4ae3'),
-('558973f51600d', 'Boot Loading', '558973f526f9d'),
-('558973f51600d', ' Boot Record', '558973f526fb9'),
-('558973f51600d', ' Boot Strapping', '558973f526fc5'),
-('558973f51600d', ' Booting', '558973f526fce'),
-('558973f55d269', ' Quick boot', '558973f57aef1'),
-('558973f55d269', 'Cold boot', '558973f57af07'),
-('558973f55d269', ' Hot boot', '558973f57af17'),
-('558973f55d269', ' Fast boot', '558973f57af27'),
-('558973f5abb1a', 'bash', '558973f5e7623'),
-('558973f5abb1a', ' Csh', '558973f5e7636'),
-('558973f5abb1a', ' ksh', '558973f5e7640'),
-('558973f5abb1a', ' sh', '558973f5e764a'),
-('5589751a63091', 'q', '5589751a81bd6'),
-('5589751a63091', 'wq', '5589751a81be8'),
-('5589751a63091', ' both (a) and (b)', '5589751a81bf4'),
-('5589751a63091', ' none of the mentioned', '5589751a81bfd'),
-('5589751ad32b8', ' moves screen down one page', '5589751adbdbd'),
-('5589751ad32b8', 'moves screen up one page', '5589751adbdce'),
-('5589751ad32b8', 'moves screen up one line', '5589751adbdd8'),
-('5589751ad32b8', ' moves screen down one line', '5589751adbde2'),
-('5589751b304ef', ' yy', '5589751b3b04d'),
-('5589751b304ef', 'yw', '5589751b3b05e'),
-('5589751b304ef', 'yc', '5589751b3b069'),
-('5589751b304ef', ' none of the mentioned', '5589751b3b073'),
-('5589751b749c9', 'X', '5589751b9a98c'),
-('5589751b749c9', 'x', '5589751b9a9a5'),
-('5589751b749c9', 'D', '5589751b9a9b7'),
-('5589751b749c9', 'd', '5589751b9a9c9'),
-('5589751bd02ec', 'autoindentation is not possible in vi editor', '5589751bdadaa');
+('5fba1c97c18a9', 'Random Access Memory', '5fba1c97c1fe6'),
+('5fba1c97c18a9', 'Randomly Access Memory', '5fba1c97c1fef'),
+('5fba1c97c18a9', 'Run Accept Memory', '5fba1c97c1ff0'),
+('5fba1c97c18a9', 'None of these', '5fba1c97c1ff1'),
+('5fba1c97c4b5a', 'Central Program Unit', '5fba1c97c50cd'),
+('5fba1c97c4b5a', 'Central Processing Unit', '5fba1c97c50cf'),
+('5fba1c97c4b5a', 'Central Preload Unit', '5fba1c97c50d0'),
+('5fba1c97c4b5a', 'None of these', '5fba1c97c50d1'),
+('5fba1c97c7468', 'Electronic Mail', '5fba1c97c828e'),
+('5fba1c97c7468', 'Electric Mail', '5fba1c97c8291'),
+('5fba1c97c7468', 'Engine Mail', '5fba1c97c8292'),
+('5fba1c97c7468', 'None of these', '5fba1c97c8293');
 
 -- --------------------------------------------------------
 
@@ -211,24 +132,9 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
-('558920ff906b8', '55892169bf6a7', 'what is command for changing user information??', 4, 1),
-('558920ff906b8', '5589216a3646e', 'what is permission for view only for other??', 4, 2),
-('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
-('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
-('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
-('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
-('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
-('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2),
-('55897338a6659', '558973f4389ac', 'On Linux, initrd is a file', 4, 1),
-('55897338a6659', '558973f4c46f2', 'Which is loaded into memory when system is booted?', 4, 2),
-('55897338a6659', '558973f51600d', ' The process of starting up a computer is known as', 4, 3),
-('55897338a6659', '558973f55d269', ' Bootstrapping is also known as', 4, 4),
-('55897338a6659', '558973f5abb1a', 'The shell used for Single user mode shell is:', 4, 5),
-('5589741f9ed52', '5589751a63091', ' Which command is used to close the vi editor?', 4, 1),
-('5589741f9ed52', '5589751ad32b8', ' In vi editor, the key combination CTRL+f', 4, 2),
-('5589741f9ed52', '5589751b304ef', ' Which vi editor command copies the current line of the file?', 4, 3),
-('5589741f9ed52', '5589751b749c9', ' Which command is used to delete the character before the cursor location in vi editor?', 4, 4),
-('5589741f9ed52', '5589751bd02ec', ' Which one of the following statement is true?', 4, 5);
+('5fba1bacc85f0', '5fba1c97c18a9', 'What is the full form of RAM?', 4, 1),
+('5fba1bacc85f0', '5fba1c97c4b5a', 'What is the full form of CPU?', 4, 2),
+('5fba1bacc85f0', '5fba1c97c7468', 'What is the full form of E-mail', 4, 3);
 
 -- --------------------------------------------------------
 
@@ -245,7 +151,7 @@ CREATE TABLE `quiz` (
   `time` bigint(20) NOT NULL,
   `intro` text NOT NULL,
   `tag` varchar(100) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -253,34 +159,7 @@ CREATE TABLE `quiz` (
 --
 
 INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
-('558920ff906b8', 'Linux : File Managment', 2, 1, 2, 5, '', 'linux', '2015-06-23 09:03:59'),
-('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2015-06-23 09:06:12'),
-('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2015-06-23 09:09:03'),
-('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2015-06-23 09:12:12'),
-('55897338a6659', 'Linux:startup', 2, 1, 5, 10, '', 'linux', '2015-06-23 14:54:48'),
-('5589741f9ed52', 'Linux :vi Editor', 2, 1, 5, 10, '', 'linux', '2015-06-23 14:58:39');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rank`
---
-
-CREATE TABLE `rank` (
-  `email` varchar(50) NOT NULL,
-  `score` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `rank`
---
-
-INSERT INTO `rank` (`email`, `score`, `time`) VALUES
-('sunnygkp10@gmail.com', 9, '2015-06-24 03:22:38'),
-('avantika420@gmail.com', 8, '2015-06-23 14:49:39'),
-('mi5@hollywood.com', 4, '2015-06-23 15:12:56'),
-('nik1@gmail.com', 1, '2015-06-23 16:11:50');
+('5fba1bacc85f0', 'Computer Science ', 1, 0, 3, 10, '', '#cs', '2020-11-22 08:05:00');
 
 -- --------------------------------------------------------
 
@@ -290,7 +169,10 @@ INSERT INTO `rank` (`email`, `score`, `time`) VALUES
 
 CREATE TABLE `user` (
   `name` varchar(50) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `college` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `mob` bigint(20) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -298,16 +180,12 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`name`, `email`, `password`) VALUES
-('Avantika', 'avantika420@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-('Mark Zukarburg',  'ceo@facebook.com',  'e10adc3949ba59abbe56e057f20f883e'),
-('Komal',  'komalpd2011@gmail.com','e10adc3949ba59abbe56e057f20f883e'),
-('Tom Cruze',  'mi5@hollywood.com', 'e10adc3949ba59abbe56e057f20f883e'),
-('Netcamp',  'netcamp@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
-('Nikunj',  'nik1@gmail.com',  '202cb962ac59075b964b07152d234b70'),
-('Sunny',  'sunnygkp10@gmail.com',  'e10adc3949ba59abbe56e057f20f883e'),
-('User', 'user@user.com',  'e10adc3949ba59abbe56e057f20f883e'),
-('Vikash',  'vikash@gmail.com',  'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Harish ', 'M', 'ASEB ', 'harish@gmail.com', 321654987, 'harish'),
+('Meghana', 'F', 'ASEB', 'meghs@gmail.com', 123456789, 'meghs '),
+('Navya ', 'F', 'Amrita School of Engineering ', 'navya@gmail.com', 1234567895, '1234'),
+('Raghu', 'M', 'ASEB', 'raghu@gmail.com', 987654321, 'raghu'),
+('Shruthi ', 'F', 'Amrita School of Engineering ', 'shruthisk2000@gmail.com', 7899362434, '1234');
 
 --
 -- Indexes for dumped tables
@@ -334,6 +212,8 @@ ALTER TABLE `user`
 --
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
