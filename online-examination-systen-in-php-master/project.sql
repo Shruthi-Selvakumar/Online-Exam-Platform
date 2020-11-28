7e582d76de9adee572afd5c1c6f0725d2472a513
Meghana Rao <meghanarao.99@gmail.com>
2020-11-28 18:52:00 +0530

    Update project.sql

--- online-examination-systen-in-php-master/project.sql
+++ online-examination-systen-in-php-master/project.sql
@@ -11,43 +11,89 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
 CREATE TABLE `admin` (
   `admin_id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `admin`
+--
+
 INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
 (1, 'admin@admin.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
 
 CREATE TABLE `answer` (
   `qid` text NOT NULL,
   `ansid` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `answer`
+--
+
 INSERT INTO `answer` (`qid`, `ansid`) VALUES
 ('5fc102e61d304' , '5fc102e61e27f') ,
 ('5fc102e6219d4', '5fc102e6222b8');
+-- --------------------------------------------------------
+
+-- Table structure for table `history`
+--
 
 CREATE TABLE `history` (
   `email` varchar(50) NOT NULL,
   `eid` text NOT NULL,
   `score` int(11) NOT NULL,
-  --level = number of questions
   `level` int(11) NOT NULL,
   `correct` int(11) NOT NULL,
   `wrong` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `history`
+--
+
 INSERT INTO `history` (`email`, `eid`, `score`, `level`, `correct`, `wrong`, `date`) VALUES
 ('admin@admin.com' ,  '5fc102500489b' , 8 ,2 ,2 ,0 , '2020-11-27 19:24:29');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
 
 CREATE TABLE `options` (
   `qid` varchar(50) NOT NULL,
   `option` varchar(5000) NOT NULL,
   `optionid` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `options`
+--
 
 INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
 ('5fc102e61d304' ,'#' ,'5fc102e61e273'),
@@ -60,6 +106,13 @@ INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
 ('5fc102e6219d4' , 'comp' , '5fc102e6222b9'),
 ('5fc102e6219d4','vc++' , '5fc102e6222ba');
 
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
 CREATE TABLE `questions` (
   `eid` text NOT NULL,
   `qid` text NOT NULL,
@@ -68,9 +121,18 @@ CREATE TABLE `questions` (
   `sn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `questions`
+--
+
 INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
 ('5fc102500489b','5fc102e61d304' , 'Which operator is used to continue the definition ...' , 4 , 1),
 ('5fc102500489b' ,'5fc102e6219d4' , 'Identify the C compiler of UNIX.' ,4 , 2);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
 
 CREATE TABLE `quiz` (
   `eid` text NOT NULL,
@@ -80,11 +142,23 @@ CREATE TABLE `quiz` (
   `total` int(11) NOT NULL,
   `time` bigint(20) NOT NULL,
   `intro` text NOT NULL,
+  `tag` varchar(100) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `quiz` (`eid`, `title`, `correct`, `wrong`, `total`, `time`, `intro`, `date`) VALUES
-('5fc102500489b' , 'Computer Science' ,4 , 1 , 2 , 5 ,'midsem', '2020-11-27 19:12:40');
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `correct`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
+('5fc102500489b' , 'Computer Science' ,4 , 1 , 2 , 5 ,'midsem','no tag', '2020-11-27 19:12:40');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
 
 CREATE TABLE `rank` (
   `email` varchar(50) NOT NULL,
@@ -92,6 +166,17 @@ CREATE TABLE `rank` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `rank`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
   `name` varchar(50) NOT NULL,
@@ -102,15 +187,39 @@ CREATE TABLE `user` (
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `user`
+--
+
 INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
 ('Meghana Rao','F','fg','meghanarao.99@gmail.com', 9535410456, '827ccb0eea8a706c4c34a16891f84e7b');
 
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`email`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
 ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
